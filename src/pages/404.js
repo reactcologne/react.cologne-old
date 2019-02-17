9dfc05817df5226b6068819378fe5fb58ac8a514
Philipp Brumm <philipp.brumm@gmail.com>
2019-02-17 15:56:28 +0100

    revert 404 route

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,14 @@
-const NotFoundPage = () => {
-  window.location = '/'
-}
+import React from 'react'
+
+import Layout from '../components/Layout'
+import SEO from '../components/Seo'
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
+)
 
 export default NotFoundPage
